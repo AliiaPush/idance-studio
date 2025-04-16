9d7eb24b1f0163193b24e61bad7a7abff6130d99
Luke Fernando <isanjaya139@gmail.com>
2025-04-16 10:31:25 +0530

    Header updated

--- api/index.php
+++ api/index.php
@@ -1,16 +1,9 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Max-Age: 86400");
-    http_response_code(204);
-    exit();
-}
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 
