b0d4399e3162556d7a2cb495c8bcd3841d23c073
Luke Fernando <isanjaya139@gmail.com>
2025-04-16 08:57:22 +0530

    Header authorizations removed

--- api/idance_studio.php
+++ api/idance_studio.php
@@ -8,7 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: text/plain");
 require_once "vendor/autoload.php";
 
 
