0ebd23c9889245c5c69c43151ad9f661e36ca8c1
Luke Fernando <isanjaya139@gmail.com>
2025-04-16 09:03:57 +0530

    Header updated

--- api/idance_studio.php
+++ api/idance_studio.php
@@ -2,15 +2,15 @@
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Max-Age: 86400");
-    exit(0);
+    http_response_code(204);
+    exit();
 }
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: text/plain");
 require_once "vendor/autoload.php";
 
 
