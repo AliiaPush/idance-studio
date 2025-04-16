0006943af6d9663aa85dab3b86bf4b2d1c97768c
Luke Fernando <isanjaya139@gmail.com>
2025-04-16 10:16:54 +0530

    autoload path updated

--- api/index.php
+++ api/index.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-require_once "vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 
 use PHPMailer\PHPMailer\PHPMailer;
