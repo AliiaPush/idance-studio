6728f40c5c1cbe43f139d2cdda3896063a8bd57f
Luke Fernando <isanjaya139@gmail.com>
2025-04-16 11:17:56 +0530

    Vercel php version roll back to the newest

--- api/index.php
+++ api/index.php
@@ -1,8 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Max-Age: 86400");
+    http_response_code(204);
+    exit();
+}
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: text/plain");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(["error" => "Method Not Allowed"]);
+    exit();
+}
+
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
